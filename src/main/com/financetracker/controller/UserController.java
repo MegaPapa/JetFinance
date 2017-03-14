688f9be85aa8c1dd6a67dff1a10c2e6d1060309e
Maxim Petrusevich <maks1464@gmail.com>
2017-03-15 01:44:14 +0300

    + log4j logger

--- src/main/com/financetracker/controller/UserController.java
+++ src/main/com/financetracker/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.financetracker.controller;
 
-import com.financetracker.bean.UserBean;
 import com.financetracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.sql.Timestamp;
 
 /**
  * Created by User on 13.03.2017.
